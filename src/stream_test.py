585abb3bc10bc0b364ff32577849f35aa9807f0f
shouryank <64479956+shouryank@users.noreply.github.com>
2021-12-04 15:52:22 +0530

    Update stream_test.py

--- src/stream_test.py
+++ src/stream_test.py
@@ -1,12 +1,24 @@
 from pyspark.context import SparkContext
 from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession
+import json
 
 # Initialize the spark context.
 sc = SparkContext(appName="ScamStreaming")
 ssc = StreamingContext(sc, 1)
 
+spark = SparkSession(sc)
+
+schema = ['feature0' , 'feature1', 'feature2']
+
+def func(rdd):
+    if len(rdd.collect()):
+        df = spark.createDataFrame(json.loads(rdd.collect()[0]).values() , schema = schema)
+        print(df)
+
 lines = ssc.socketTextStream("localhost", 6100)
-lines.pprint()
+lines.foreachRDD(func)
 
 ssc.start()
 ssc.awaitTermination()
+ssc.stop()
