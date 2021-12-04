c7a3ccd3e382c028d8540f5d162a08844a7909a6
shouryank <64479956+shouryank@users.noreply.github.com>
2021-12-04 17:34:29 +0530

    Update stream_test.py

--- src/stream_test.py
+++ src/stream_test.py
@@ -1,6 +1,7 @@
 from pyspark.context import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
 import json
 
 # Initialize the spark context.
@@ -9,9 +10,12 @@ ssc = StreamingContext(sc, 5)
 
 spark = SparkSession(sc)
 
+schema = StructType([StructField("feature0", StringType(), True), StructField("feature1", StringType(), True), StructField("feature2", StringType(), True)])
+
 def func(rdd):
     if len(rdd.collect()):        
-        df = spark.createDataFrame(rdd)
+        df = spark.createDataFrame(json.loads(rdd.collect()[0]).values(), schema)
+        df.select("feature0").show()
 
 lines = ssc.socketTextStream("localhost", 6100)
 lines.foreachRDD(func)
