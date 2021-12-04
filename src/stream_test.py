570887e0108b9909cb5d4f0a7351de85e80e628c
ShattereDarkness <60350202+ShattereDarkness@users.noreply.github.com>
2021-12-04 23:52:50 +0530

    Made alpha numeric work

--- src/stream_test.py
+++ src/stream_test.py
@@ -2,6 +2,7 @@ from pyspark.context import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import col, udf, struct
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, StringIndexer
 from pyspark.ml import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
@@ -18,22 +19,22 @@ spark = SparkSession(sc)
 
 schema = StructType([StructField("feature0", StringType(), True), StructField("feature1", StringType(), True), StructField("feature2", StringType(), True)])
 
-def removeNonAlphaNumeric(rdd):
-    l = rdd.collect()
-
-    if len(l):
-        regex = re.compile('[^a-zA-Z]')
-        rdd.map(lambda i: regex.sub('', i))
-    
-    return rdd
+def removeNonAlphaNumeric(s):
+    regex = re.compile('[^a-zA-Z\s]')
+    s = regex.sub('', s)   
+    return s
 
 def func(rdd):
     l = rdd.collect()
 
-    if len(l): 
-        rdd = removeNonAlphaNumeric(rdd)
-
+    if len(l):
         df = spark.createDataFrame(json.loads(rdd.collect()[0]).values(), schema)
+
+        remove_alpha = udf(removeNonAlphaNumeric, StringType())
+        new_df = df.withColumn("feature0", remove_alpha(df["feature0"]))
+        new_df.select('feature0').show()
+
+        # df.show()
         
         # tokenizer = Tokenizer(inputCol="feature0", outputCol="words") 
 
@@ -43,11 +44,11 @@ def func(rdd):
         # cleaner = data_prep_pipe.fit(df)
         # clean_data = cleaner.transform(df)
 
-        df.select('feature0').show()
-
+        # df.select('feature0').show()
+        # new_data = df.collect() #[[]]
         # l = []
         # for row in new_data:
-        #     l.append(row['feature0'])
+        #     l.append(row['feature0']) #['sayo', 'sucks']
 
         # vectorizer = CountVectorizer()
         # X = vectorizer.fit_transform(l)
