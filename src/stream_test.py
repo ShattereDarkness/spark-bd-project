3062df873037cb7053363832898c4fb6acd7c589
shouryank <64479956+shouryank@users.noreply.github.com>
2021-12-04 19:25:30 +0530

    Update stream_test.py

--- src/stream_test.py
+++ src/stream_test.py
@@ -2,6 +2,10 @@ from pyspark.context import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, StringIndexer
+from pyspark.ml import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
 import json
 
 # Initialize the spark context.
@@ -15,7 +19,29 @@ schema = StructType([StructField("feature0", StringType(), True), StructField("f
 def func(rdd):
     if len(rdd.collect()):        
         df = spark.createDataFrame(json.loads(rdd.collect()[0]).values(), schema)
-        df.select("feature0").show()
+        
+        # tokenizer = Tokenizer(inputCol="feature0", outputCol="words") 
+
+        # stopremove = StopWordsRemover(inputCol='words',outputCol='stop_tokens')
+
+        # data_prep_pipe = Pipeline(stages=[tokenizer, stopremove])
+        # cleaner = data_prep_pipe.fit(df)
+        # clean_data = cleaner.transform(df)
+
+        new_data = df.collect()
+
+        # l = []
+        # for row in new_data:
+        #     l.append(row['feature0'])
+
+        # vectorizer = CountVectorizer()
+        # X = vectorizer.fit_transform(l)
+        # X_arr =  X.toarray()
+
+        # le = LabelEncoder()
+        # y = le.fit(new_data["feature2"])
+
+        print(new_data[2])
 
 lines = ssc.socketTextStream("localhost", 6100)
 lines.foreachRDD(func)
