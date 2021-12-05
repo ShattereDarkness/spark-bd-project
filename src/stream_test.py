28545fb89a1f82c25b78a4eeb3785ad09b46bedf
shouryank <64479956+shouryank@users.noreply.github.com>
2021-12-05 18:55:40 +0530

    Update stream_test.py

--- src/stream_test.py
+++ src/stream_test.py
@@ -7,8 +7,9 @@ from pyspark.sql.functions import udf
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import Perceptron, SGDClassifier
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.metrics import confusion_matrix
 
@@ -28,8 +29,9 @@ schema = StructType([StructField("feature0", StringType(), True), StructField("f
 vectorizer = HashingVectorizer(alternate_sign=False)
 le = LabelEncoder()
 mnb = MultinomialNB()
-gnb = GaussianNB()
+sgd = SGDClassifier()
 per = Perceptron()
+kmeans = MiniBatchKMeans(n_clusters=2)
 X = None
 data = None
 batch_size = 1500
@@ -54,7 +56,7 @@ def func(rdd):
 
         y = le.fit_transform(np.array([x['feature2']  for x in df_list]))
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y,  random_state = 9)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state = 9)
 
         #multinomial nb
         model = mnb.partial_fit(X_train, y_train, classes = np.unique(y_train))
@@ -65,42 +67,56 @@ def func(rdd):
         recall = recall_score(y_test, pred)
         conf_m = confusion_matrix(y_test, pred)
 
-        print(f"accuracy: %.3f" %accuracy)
+        print(f"\nmulti : accuracy: %.3f" %accuracy)
         print(f"precision: %.3f" %precision)
         print(f"recall: %.3f" %recall)
         print(f"confusion matrix: ")
         print(conf_m)
 
-        # #gaussiannb
-        # model = gnb.partial_fit(X_train, y_train, classes = np.unique(y_train))
-        # pred = model.predict(X_test)
+        #perceptron
+        model = per.partial_fit(X_train, y_train, classes = np.unique(y_train))
+        pred = model.predict(X_test)
 
-        # accuracy = accuracy_score(y_test, pred)
-        # precision = precision_score(y_test, pred)
-        # recall = recall_score(y_test, pred)
-        # conf_m = confusion_matrix(y_test, pred)
+        accuracy = accuracy_score(y_test, pred)
+        precision = precision_score(y_test, pred)
+        recall = recall_score(y_test, pred)
+        conf_m = confusion_matrix(y_test, pred)
 
-        # print(f"accuracy: %.3f" %accuracy)
-        # print(f"precision: %.3f" %precision)
-        # print(f"recall: %.3f" %recall)
-        # print(f"confusion matrix: ")
-        # print(conf_m)
+        print(f"\nperceptron : accuracy: %.3f" %accuracy)
+        print(f"precision: %.3f" %precision)
+        print(f"recall: %.3f" %recall)
+        print(f"confusion matrix: ")
+        print(conf_m)
 
-        # #perceptron
-        # model = per.partial_fit(X_train, y_train, classes = np.unique(y_train))
-        # pred = model.predict(X_test)
+        # sgdclassifier
+        model = sgd.partial_fit(X_train, y_train, classes = np.unique(y_train))
+        pred = model.predict(X_test)
 
-        # accuracy = accuracy_score(y_test, pred)
-        # precision = precision_score(y_test, pred)
-        # recall = recall_score(y_test, pred)
-        # conf_m = confusion_matrix(y_test, pred)
+        accuracy = accuracy_score(y_test, pred)
+        precision = precision_score(y_test, pred)
+        recall = recall_score(y_test, pred)
+        conf_m = confusion_matrix(y_test, pred)
 
-        # print(f"accuracy: %.3f" %accuracy)
-        # print(f"precision: %.3f" %precision)
-        # print(f"recall: %.3f" %recall)
-        # print(f"confusion matrix: ")
-        # print(conf_m)
+        print(f"\nsgd : accuracy: %.3f" %accuracy)
+        print(f"precision: %.3f" %precision)
+        print(f"recall: %.3f" %recall)
+        print(f"confusion matrix: ")
+        print(conf_m)
 
+        #k means clustering
+        model = kmeans.partial_fit(X_train, y_train)
+        pred = model.predict(X_test)
+
+        accuracy = accuracy_score(y_test, pred)
+        precision = precision_score(y_test, pred)
+        recall = recall_score(y_test, pred)
+        conf_m = confusion_matrix(y_test, pred)
+
+        print(f"\nk-means : accuracy: %.3f" %accuracy)
+        print(f"precision: %.3f" %precision)
+        print(f"recall: %.3f" %recall)
+        print(f"confusion matrix: ")
+        print(conf_m)
 
 lines = ssc.socketTextStream("localhost", 6100)
 lines.foreachRDD(func)
